990dc9a20634799c0c0045cd90daf8b111470b30
Emad Roghani <emadroghani@gmail.com>
2021-03-05 21:08:40 +0330

    add a paragraph to index.hbs

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,13 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const p1 = document.querySelector("#message-1");
 const p2 = document.querySelector("#message-2");
+const p3 = document.querySelector("#message-3");
 
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   p1.textContent = "loading...";
   p2.textContent = "";
+  p3.textContent = "";
   const location = search.value;
 
   fetch("/weather?address=" + location).then((response) => {
@@ -16,6 +18,7 @@ weatherForm.addEventListener("submit", (event) => {
       }
       p1.textContent = data.location;
       p2.textContent = data.forecast;
+      p3.textContent = "The datas are came from a free weather api";
     });
   });
 });
